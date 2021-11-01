da1ea80e6afb7584352daabbd3b5260d60827d54
Bruno C <brunocatania007@gmail.com>
2021-11-01 12:39:55 -0300

    Segundo commit

--- src/libreria/entidades/Autor.java
+++ src/libreria/entidades/Autor.java
@@ -1,7 +1,6 @@
 package libreria.entidades;
 
 import com.sun.istack.internal.NotNull;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
